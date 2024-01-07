ac382ab26720052a42cd72cc1e75e994d0e765da
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-07 15:33:57 +0530

    Auto format

--- code/datasets/deepfake_ecg/Deepfake_ECG_Dataset.py
+++ code/datasets/deepfake_ecg/Deepfake_ECG_Dataset.py
@@ -1,6 +1,7 @@
 import torch
 import pandas
 
+
 class Deepfake_ECG_Dataset(torch.utils.data.Dataset):
     """
     Deepfake ECG dataset filtered to only include normal ECGs
@@ -8,39 +9,49 @@ class Deepfake_ECG_Dataset(torch.utils.data.Dataset):
     ECG signals are returned as a 1D tensor (40k numbers)
     Parameters are returned as a 1D tensor
     """
+
     def __init__(self):
         super(Deepfake_ECG_Dataset, self).__init__()
-        
+
         # load the ground truth labels
-        self.ground_truths = pandas.read_csv('datasets/deepfake_ecg/filtered_all_normals_121977_ground_truth.csv')
-        
+        self.ground_truths = pandas.read_csv(
+            "datasets/deepfake_ecg/filtered_all_normals_121977_ground_truth.csv"
+        )
+
         # TODO: take the column name as input and use it to select the column
         # hardcode for now to only get the HR
-        RR = torch.tensor(self.ground_truths['avgrrinterval'].values, dtype=torch.float32)
+        RR = torch.tensor(
+            self.ground_truths["avgrrinterval"].values, dtype=torch.float32
+        )
         # calculate HR
-        self.heart_rate = 60 * 1000/RR
+        self.heart_rate = 60 * 1000 / RR
 
     def __getitem__(self, index):
-        filename= self.ground_truths['patid'].values[index]
-        ecg_signals = pandas.read_csv(f'datasets/deepfake_ecg/from_006_chck_2500_150k_filtered_all_normals_121977/{filename}.asc',header=None, sep=" ")
-        
-        ecg_signals = torch.tensor(ecg_signals.values) # convert dataframe values to tensor
-        
+        filename = self.ground_truths["patid"].values[index]
+        ecg_signals = pandas.read_csv(
+            f"datasets/deepfake_ecg/from_006_chck_2500_150k_filtered_all_normals_121977/{filename}.asc",
+            header=None,
+            sep=" ",
+        )
+
+        ecg_signals = torch.tensor(
+            ecg_signals.values
+        )  # convert dataframe values to tensor
+
         ecg_signals = ecg_signals.float()
-        
+
         ecg_signals = ecg_signals.reshape(-1)
-        
+
         # Transposing the ecg signals
         # ecg_signals = ecg_signals/6000 # normalization
-        ecg_signals = ecg_signals.t() 
-        
+        ecg_signals = ecg_signals.t()
+
         # Reshape ecg_signals into a 1D array
-        
+
         heart_rate = self.heart_rate[index].reshape(-1)
-        
+
         return ecg_signals, heart_rate
 
     def __len__(self):
-        # return 1000
-        return self.ground_truths.shape[0]
-    
+        return 5000
+        # return self.ground_truths.shape[0]
--- code/models/SimpleNeuralNetwork.py
+++ code/models/SimpleNeuralNetwork.py
@@ -1,15 +1,33 @@
 import torch.nn as nn
 
+
 class SimpleNeuralNetwork(nn.Module):
     def __init__(self):
         super(SimpleNeuralNetwork, self).__init__()
         self.all = nn.Sequential(
-            nn.Linear(40000, 1000),
+            nn.Linear(40000, 20000),
+            nn.ReLU(),
+            nn.Linear(20000, 10000),
+            nn.ReLU(),
+            nn.Linear(10000, 5000),
+            nn.ReLU(),
+            nn.Linear(5000, 2500),
+            nn.ReLU(),
+            nn.Linear(2500, 1000),
             nn.ReLU(),
             nn.Linear(1000, 500),
             nn.ReLU(),
-            nn.Linear(500, 1),
+            nn.Linear(500, 100),
+            nn.ReLU(),
+            nn.Linear(100, 10),
+            nn.ReLU(),
+            nn.Linear(10, 1),
         )
+        # self.all = nn.Sequential(
+        #     nn.Linear(40000, 10),
+        #     nn.ReLU(),
+        #     nn.Linear(10, 1),
+        # )
 
     def forward(self, x):
         x = self.all(x)
