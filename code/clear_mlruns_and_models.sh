b847a6507bcaea3dfdaec52edb7c317960ffe4d4
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-08 08:37:05 +0530

    Fix typo

--- code/clear_mlruns_and_models.sh
+++ code/clear_mlruns_and_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Delete mlruns and saved scripts
+# Delete mlruns and saved models 
 
 # Prompt the user for confirmation
 read -p "Are you sure that you want to delete everything in mlruns and saved_models (This will delete all the training logs and the saved model files)? (YES/NO): " response
