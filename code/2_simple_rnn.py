43a079712f5524fa9d6a57192afd9118dc449ece
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-09 18:37:22 +0530

    Change from mlflow into wandb

--- code/2_simple_rnn.py
+++ code/2_simple_rnn.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from tqdm import tqdm
-import mlflow
-import mlflow.pytorch
 import datetime
+import wandb
+import os
 
 from models.SimpleRNN import SimpleRNN
 from datasets.deepfake_ecg.Deepfake_ECG_Dataset import Deepfake_ECG_Dataset
@@ -12,8 +11,21 @@ from datasets.deepfake_ecg.Deepfake_ECG_Dataset import Deepfake_ECG_Dataset
 # Hyperparameters
 batch_size = 32
 learning_rate = 0.01
-num_epochs = 200
+num_epochs = 2000
 train_fraction = 0.8
+
+# start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="initial-testing",
+    # track hyperparameters and run metadata
+    config={
+        "learning_rate": learning_rate,
+        "architecture": os.path.basename(__file__),
+        "dataset": "Deepfake_ECG_Dataset",
+        "epochs": num_epochs,
+    },
+)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -42,73 +54,63 @@ val_dataloader = torch.utils.data.DataLoader(
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 criterion = nn.L1Loss()
 
-# MLflow setup
-mlflow.set_experiment("SimpleRNN")
-with mlflow.start_run():
-    # Log hyperparameters
-    mlflow.log_param("batch_size", batch_size)
-    mlflow.log_param("learning_rate", learning_rate)
-    mlflow.log_param("num_epochs", num_epochs)
 
-    # Training loop
-    for epoch in range(num_epochs):
-        model.train()
-        running_loss = 0.0
+# Training loop
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for i, data in tqdm(
+        enumerate(train_dataloader, 0),
+        total=len(train_dataloader),
+        desc=f"Training Epoch {epoch + 1}/{num_epochs}",
+    ):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    # Log metrics
+    wandb.log({"train_loss": running_loss / len(train_dataloader)})
+    print(f"Epoch: {epoch} train_loss: {running_loss / len(train_dataloader)}")
+
+    # Validation loop
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
         for i, data in tqdm(
-            enumerate(train_dataloader, 0),
-            total=len(train_dataloader),
-            desc=f"Training Epoch {epoch + 1}/{num_epochs}",
+            enumerate(val_dataloader, 0),
+            total=len(val_dataloader),
+            desc=f"Validating Epoch {epoch + 1}/{num_epochs}",
         ):
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
 
-            optimizer.zero_grad()
             outputs = model(inputs)
+            # if i == 0:
+            #     for x in range(len(outputs)):
+            #         print(f"Predicted: {outputs[x]} Real: {labels[x]}")
             loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
 
             running_loss += loss.item()
 
         # Log metrics
-        mlflow.log_metric(
-            "train_loss", running_loss / len(train_dataloader), step=epoch
-        )
-        print(f"Epoch: {epoch} train_loss: {running_loss / len(train_dataloader)}")
+        wandb.log({"val_loss": running_loss / len(val_dataloader)})
 
-        # Validation loop
-        model.eval()
-        running_loss = 0.0
-        with torch.no_grad():
-            for i, data in tqdm(
-                enumerate(val_dataloader, 0),
-                total=len(val_dataloader),
-                desc=f"Validating Epoch {epoch + 1}/{num_epochs}",
-            ):
-                inputs, labels = data
-                inputs, labels = inputs.to(device), labels.to(device)
+    print(f"Epoch: {epoch} val_loss: {running_loss / len(val_dataloader)}")
 
-                outputs = model(inputs)
-                # if i == 0:
-                #     for x in range(len(outputs)):
-                #         print(f"Predicted: {outputs[x]} Real: {labels[x]}")
-                loss = criterion(outputs, labels)
-
-                running_loss += loss.item()
-
-            # Log metrics
-            mlflow.log_metric(
-                "val_loss", running_loss / len(val_dataloader), step=epoch
-            )
-
-        print(f"Epoch: {epoch} val_loss: {running_loss / len(val_dataloader)}")
-
-    # # Save the trained model with date and time in the path
-    # current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    # model_path = f"saved_models/{current_time}"
-    # mlflow.pytorch.save_model(model, model_path)
+# Save the trained model with date and time in the path
+current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+model_path = f"saved_models/{current_time}"
+torch.save(model, model_path)
 
 print("Finished Training")
+wandb.finish()
 
 # create a backup of mlruns in babbage server
 # "Turing is not stable, data could be lost" - Akila E17
--- code/env_install.sh
+++ code/env_install.sh
@@ -4,5 +4,9 @@
 pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu118
 pip3 install pandas==2.1.4
 pip3 install matplotlib==3.8.2
-pip3 install mlflow==2.9.2
 pip3 install tqdm
+pip install wandb
+
+# after that 
+# 'wandb login' 
+# then paste your api key
