fe49e6a29b2e309a9733b6f2bc856049767340e1
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-09 18:36:07 +0530

    Add PTB-XL and PTB-XL+ download scripts

---
+++ code/datasets/PTB-XL-Plus/download.sh
@@ -0,0 +1,3 @@
+wget https://physionet.org/static/published-projects/ptb-xl-plus/ptb-xl-a-comprehensive-electrocardiographic-feature-dataset-1.0.1.zip
+unzip ptb-xl-a-comprehensive-electrocardiographic-feature-dataset-1.0.1.zip
+rm ptb-xl-a-comprehensive-electrocardiographic-feature-dataset-1.0.1.zip
---
+++ code/datasets/PTB-XL/download.sh
@@ -0,0 +1,3 @@
+wget https://physionet.org/static/published-projects/ptb-xl/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.3.zip
+unzip ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.3.zip
+rm ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.3.zip
