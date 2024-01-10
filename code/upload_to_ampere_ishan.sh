f292e55f6259c1f9c944f10864fdffea59149bde
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-10 09:28:07 +0530

    Fix upload to server scripts to exlude dataset files

--- code/upload_to_ampere_ishan.sh
+++ code/upload_to_ampere_ishan.sh
@@ -1,2 +1,2 @@
 # upload the code files to ampere server
-rsync -avz --exclude='**/*.csv' --exclude='**/*.asc' --exclude='**/*.pyc' --exclude='saved_models/*' --exclude='mlruns/*' -e "ssh -i /home/ishanfdo/.ssh/id_rsa -J e18098@aiken.ce.pdn.ac.lk" * e18098@10.40.18.10:/storage/scratch1/e18-4yp-comp-ecg-analysis
+rsync -avz --exclude='**/*.csv' --exclude='**/*.dat' --exclude='**/*.hea' --exclude='**/*.atr' --exclude='**/*.asc' --exclude='**/*.pyc' --exclude='saved_models/*' --exclude='mlruns/*' -e "ssh -i /home/ishanfdo/.ssh/id_rsa -J e18098@aiken.ce.pdn.ac.lk" * e18098@10.40.18.10:/storage/scratch1/e18-4yp-comp-ecg-analysis
--- code/upload_to_turing_ishan.sh
+++ code/upload_to_turing_ishan.sh
@@ -1,2 +1,2 @@
 # upload the code files to turing server
-rsync -avz --exclude='**/*.csv' --exclude='**/*.asc' --exclude='**/*.pyc' --exclude='saved_models/*' --exclude='mlruns/*' -e "ssh -i /home/ishanfdo/.ssh/id_rsa -J e18098@aiken.ce.pdn.ac.lk" * e18098@turing.ce.pdn.ac.lk:/storage/scratch/e18-4yp-comp-ecg-analysis
+rsync -avz --exclude='**/*.csv'  --exclude='**/*.dat' --exclude='**/*.hea' --exclude='**/*.atr'  --exclude='**/*.asc' --exclude='**/*.pyc' --exclude='saved_models/*' --exclude='mlruns/*' -e "ssh -i /home/ishanfdo/.ssh/id_rsa -J e18098@aiken.ce.pdn.ac.lk" * e18098@turing.ce.pdn.ac.lk:/storage/scratch/e18-4yp-comp-ecg-analysis
