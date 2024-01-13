bf016e1eeeb254c4887ab5a900c8377d96e1959b
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-13 06:03:50 +0530

    Update LSTM architecture

--- code/models/SimpleLSTM.py
+++ code/models/SimpleLSTM.py
@@ -4,16 +4,18 @@ import torch.nn as nn
 class SimpleLSTM(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm1 = nn.LSTM(
-            input_size=40000, hidden_size=500, num_layers=1, batch_first=True
+        self.lstm = nn.LSTM(
+            input_size=40000, hidden_size=100, num_layers=1, batch_first=True
         )
-        self.MLP1 = nn.Sequential(
-            nn.Linear(500, 1000),
+        self.MLP = nn.Sequential(
+            nn.Linear(100, 1000),
             nn.ReLU(),
-            nn.Linear(1000, 1),
+            nn.Linear(1000, 100),
+            nn.ReLU(),
+            nn.Linear(100, 1),
         )
 
     def forward(self, x):
-        x, _ = self.lstm1(x)
-        x = self.MLP1(x)
+        x, _ = self.lstm(x)
+        x = self.MLP(x)
         return x
