99e961a6cab28acd42185162659b3f46acaf8e2f
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-14 05:38:14 +0530

    Change log clearing script name

--- code/clear_wandb_and_models.sh
+++ code/clear_wandb_and_models.sh
@@ -3,12 +3,11 @@
 # Delete mlruns and saved models 
 
 # Prompt the user for confirmation
-read -p "Are you sure that you want to delete everything in mlruns and saved_models (This will delete all the training logs and the saved model files)? (YES/NO): " response
+read -p "Are you sure that you want to delete everything in wandb and saved_models (This will delete all the training logs and the saved model files)? (YES/NO): " response
 
 # Check the user's response
 if [ "$response" = "YES" ] ; then
     # Delete the directories
-    rm -rf mlruns
     rm -rf saved_models
     rm -rf wandb
     echo "Directories deleted."
