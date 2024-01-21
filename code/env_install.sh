8badd202300952b87a20c3db0acd4cc39e60d444
IshanFernando <e18098@eng.pdn.ac.lk>
2024-01-21 09:32:21 +0530

    Add versions to pip install

--- code/env_install.sh
+++ code/env_install.sh
@@ -4,9 +4,9 @@
 pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu118
 pip3 install pandas==2.1.4
 pip3 install matplotlib==3.8.2
-pip3 install tqdm
-pip install wfdb
-pip install wandb
+pip3 install tqdm==4.66.1
+pip install wfdb==4.1.2
+pip install wandb==0.16.1
 
 # after that 
 # 'wandb login' 
