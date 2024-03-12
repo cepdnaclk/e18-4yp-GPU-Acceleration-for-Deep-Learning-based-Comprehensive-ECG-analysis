5edbf0224d4fd7db6e53d103cc82f82202ee70e9
NipunFernando <e18100@eng.pdn.ac.lk>
2024-03-12 09:43:02 +0800

    Model name error

--- code/models/SimpleCNNClassification.py
+++ code/models/SimpleCNNClassification.py
@@ -1,47 +1,29 @@
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class SimpleCNNClassification(nn.Module):
     def __init__(self):
-        super(SimpleCNNClassification, self).__init__()
+        super(SimpleCNN, self).__init__()
 
         # Convolutional layers
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.relu1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv1 = nn.Conv1d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv1d(16, 1, kernel_size=3, stride=1, padding=1)
 
         # Fully connected layers
-        self.fc1 = nn.Linear(40000, 1000)
-        self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(1000, 100)
-        self.relu4 = nn.ReLU()
-        self.fc3 = nn.Linear(100, 10)
-        self.relu5 = nn.ReLU()
-        self.fc4 = nn.Linear(10, 5)
+        self.fc1 = nn.Linear(40000, 128)  
+        self.fc2 = nn.Linear(128, 5)  
 
     def forward(self, x):
-        # Convolutional layers
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
+        # Apply convolutional layers with activation functions
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
 
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
+        # Flatten the tensor for fully connected layers
+        x = x.view(x.size(0), -1)
 
-        # Flatten before fully connected layers
-        x = x.view(-1, 40000)
-
-        # Fully connected layers
-        x = self.fc1(x)
-        x = self.relu3(x)
+        # Apply fully connected layers with activation functions
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        x = self.relu4(x)
-        x = self.fc3(x)
-        x = self.relu5(x)
-        x = self.fc4(x)
 
         return x
--- code/models/SimpleNeuralNetworkClassification.py
+++ code/models/SimpleNeuralNetworkClassification.py
@@ -1,38 +1,21 @@
-# import torch.nn as nn
-
-
-# class SimpleNeuralNetworkClassification(nn.Module):
-#     def __init__(self):
-#         super(SimpleNeuralNetworkClassification, self).__init__()
-#         self.all = nn.Sequential(
-#             nn.Linear(40000, 1000),
-#             nn.ReLU(),
-#             nn.Linear(1000, 100),
-#             nn.ReLU(),
-#             nn.Linear(100, 10),
-#             nn.ReLU(),
-#             nn.Linear(10, 5),
-#         )
-
-#     def forward(self, x):
-#         x = self.all(x)
-#         return x
-
-
 import torch.nn as nn
-import torch.nn.functional as F
 
-class SimpleCNN(nn.Module):
+
+class SimpleNeuralNetworkClassification(nn.Module):
     def __init__(self):
-        super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(4000, 100, kernel_size=5)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(100, 10, kernel_size=5)
-        self.fc1 = nn.Linear(10, 5)
+        super(SimpleNeuralNetworkClassification, self).__init__()
+        self.all = nn.Sequential(
+            nn.Linear(40000, 1000),
+            nn.ReLU(),
+            nn.Linear(1000, 100),
+            nn.ReLU(),
+            nn.Linear(100, 10),
+            nn.ReLU(),
+            nn.Linear(10, 5),
+        )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 10)
-        x = self.fc1(x)
+        x = self.all(x)
         return x
+
+
