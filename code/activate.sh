7c8245a289cdc8b1e072c28ba9bac38ee3865561
IshanFernando <e18098@eng.pdn.ac.lk>
2024-04-02 08:39:44 +0530

    Add L1loss notebook and activate.sh

---
+++ code/activate.sh
@@ -0,0 +1,4 @@
+# run 'source activate.sh' to activate the conda environment
+# this is like a shortcut instead of writing the whole command below
+# type "source act" and press tab to autocomplete the command
+conda activate conda/
