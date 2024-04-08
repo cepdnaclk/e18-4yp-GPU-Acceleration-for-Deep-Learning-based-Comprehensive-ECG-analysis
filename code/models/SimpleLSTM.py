0e5125383103e7b737c2ea41a83b27cf303bea8a
ridmajayasundara <e18155@eng.pdn.ac.lk>
2024-04-08 15:07:45 +0530

    2D input added for 3_LSTM

--- code/models/SimpleLSTM.py
+++ code/models/SimpleLSTM.py
@@ -2,20 +2,27 @@ import torch.nn as nn
 
 
 class SimpleLSTM(nn.Module):
-    def __init__(self, input_size=40000):
+    def __init__(self):
         super().__init__()
         self.lstm = nn.LSTM(
-            input_size=input_size, hidden_size=100, num_layers=1, batch_first=True
+            input_size=5000, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True
         )
+        self.dropout = nn.Dropout(0.2)
+
         self.MLP = nn.Sequential(
-            nn.Linear(100, 1000),
+            nn.Linear(256, 1000),
             nn.ReLU(),
             nn.Linear(1000, 100),
             nn.ReLU(),
             nn.Linear(100, 1),
         )
+        # print("model init done")
 
     def forward(self, x):
-        x, _ = self.lstm(x)
-        x = self.MLP(x)
-        return x
+        # batch_size ,seq_len ,num_channels = x.size()
+        # x = x.view(batch_size,num_channels, seq_len) 
+        output, _ = self.lstm(x)
+        output = output[:, -1, :]  # Get the last hidden state
+        output = self.dropout(output)
+        output = self.MLP(output)
+        return output
