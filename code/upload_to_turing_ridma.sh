bcdbb5f7e701148a5bc8c4c7cd40b608cb17be60
ridmajayasundara <e18155@eng.pdn.ac.lk>
2024-04-15 10:17:59 +0530

    scripted added to upload to turing

---
+++ code/upload_to_turing_ridma.sh
@@ -0,0 +1,2 @@
+# upload the code files to turing server
+rsync -avz --exclude='**/*.csv' --exclude='wandb' --exclude='datasets/deepfake_ecg/from_006_chck_2500_150k_filtered_all_normals_121977'  --exclude='datasets/PTB_XL_Plus/ptb-xl-a-comprehensive-electrocardiographic-feature-dataset-1.0.1' --exclude='**/*.dat' --exclude='**/*.hea' --exclude='**/*.atr'  --exclude='**/*.asc' --exclude='**/*.pyc' --exclude='saved_models/*' --exclude='mlruns/*' -e "ssh -J e18155@aiken.ce.pdn.ac.lk" * e18155@turing.ce.pdn.ac.lk:/storage/scratch/e18-4yp-comp-ecg-analysis
