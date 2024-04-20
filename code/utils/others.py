79e6633331f22cf8096fac826c63f224f1866573
IshanFernando <e18098@eng.pdn.ac.lk>
2024-04-20 15:29:15 +0530

    Fix last updated by

--- code/utils/others.py
+++ code/utils/others.py
@@ -6,22 +6,8 @@ def get_latest_update_by():
     if not is_running_in_server():
         return "--Not running in servers--"
 
-    # Run the "ls -alt" command and capture its output
-    process = subprocess.run(["ls", "-alt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    output_str = process.stdout
-
-    # Split the output into lines
-    lines = output_str.strip().split("\n")
-
-    # Exclude the first line (summary) and the last line (current directory)
-    lines = lines[2]
-
-    # Loop through the lines and extract the username from the latest updated files
-    parts = lines.split()
-    username = parts[2]
-    if username.startswith("e18"):
-        return username
-
-    return "Not found"
-
-
+    try:
+        with open("last_updated_by.txt", "r") as f:
+            return f.read().strip()
+    except:
+        return "Not found"
