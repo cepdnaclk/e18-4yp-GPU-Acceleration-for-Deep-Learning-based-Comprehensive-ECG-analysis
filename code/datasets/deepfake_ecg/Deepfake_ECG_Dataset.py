8a24f8460b5b11e8c4a91dbe3de3ee1640bb10f8
IshanFernando <e18098@eng.pdn.ac.lk>
2024-04-29 18:34:48 +0530

    Auto format

--- code/datasets/deepfake_ecg/Deepfake_ECG_Dataset.py
+++ code/datasets/deepfake_ecg/Deepfake_ECG_Dataset.py
@@ -114,10 +114,10 @@ class Deepfake_ECG_Dataset(torch.utils.data.Dataset):
         # Transposing the ECG signals
         ecg_signals = ecg_signals.t()
         return ecg_signals
-    
-    def convert_to_CH_8_2D_MATRIX_OUTPUT_TYPE(self,ecg_signals):
+
+    def convert_to_CH_8_2D_MATRIX_OUTPUT_TYPE(self, ecg_signals):
         ecg_signals = torch.tensor(ecg_signals.values, dtype=torch.float32)
-        ecg_signals = torch.transpose(ecg_signals, 0, 1) # take the transpose ||| 5000,8 >>>> 8,5000
+        ecg_signals = torch.transpose(ecg_signals, 0, 1)  # take the transpose ||| 5000,8 >>>> 8,5000
         # ecg_signals = (ecg_signals + 3713.0) / 7642  # normalization : Range (0-1) # AS OF NOW, NOT BEING NORMALIZED
         return ecg_signals
 
