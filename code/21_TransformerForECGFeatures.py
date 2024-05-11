812fdec803df16b196c831a41046e9156b3e156e
NipunFernando <e18100@eng.pdn.ac.lk>
2024-05-11 16:40:20 +0800

    New train test code for features

---
+++ code/21_TransformerForECGFeatures.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+import datetime
+import wandb
+import os
+import numpy as np
+import random
+from sklearn.model_selection import train_test_split
+
+from models.Code1_Model import TransformerEncoderModel
+from datasets.deepfake_ecg.Deepfake_ECG_Dataset import Deepfake_ECG_Dataset
+import utils.current_server as current_server
+
+# Hyperparameters
+batch_size = 32
+learning_rate = 0.01
+num_epochs = 50
+train_fraction = 0.8
+patch_size = 3
+input_size = 10
+num_layers = 4
+num_heads = 8
+dim_feedforward = 2048
+output_size = 1
+
+best_model = None
+best_validation_loss = 1000000
+
+# start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="version2",
+    # track hyperparameters and run metadata
+    config={
+        "learning_rate": learning_rate,
+        "architecture": os.path.basename(__file__),
+        "dataset": "Deepfake_ECG_Dataset",
+        "epochs": num_epochs,
+    },
+)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Create the model
+model = TransformerEncoderModel(input_size, num_layers, num_heads, dim_feedforward, output_size).to(device)
+
+# Create the dataset class
+# Load all_lead_intervals here and provide it as an argument
+dataset = Deepfake_ECG_Dataset(parameter=all_lead_intervals)
+
+# Split the dataset into training and validation sets
+train_indices, val_indices = train_test_split(range(len(dataset)), test_size=1 - train_fraction, random_state=42, shuffle=True)
+
+train_dataset = torch.utils.data.Subset(dataset, train_indices)
+val_dataset = torch.utils.data.Subset(dataset, val_indices)
+
+# set num_workers
+if current_server.is_running_in_server():
+    print(f"Running in {current_server.get_current_hostname()} server, Settings num_workers to 4")
+    num_workers = 4
+else:
+    print(f"Running in {current_server.get_current_hostname()} server, Settings num_workers to 0")
+    num_workers = 0
+
+# Create data loaders for training and validation
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+# Optimizer and loss function
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+criterion = nn.L1Loss()
+
+# Learning rate scheduler
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+
+# Training loop
+for epoch in range(num_epochs):
+    model.train()
+    train_loss = 0.0
+    for i, data in tqdm(
+        enumerate(train_dataloader, 0),
+        total=len(train_dataloader),
+        desc=f"Training Epoch {epoch + 1}/{num_epochs}",
+    ):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+
+    # Validation loop
+    model.eval()
+    val_loss = 0.0
+    with torch.no_grad():
+        for i, data in tqdm(
+            enumerate(val_dataloader, 0),
+            total=len(val_dataloader),
+            desc=f"Validating Epoch {epoch + 1}/{num_epochs}",
+        ):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+
+    # Log metrics
+    wandb.log({"train_loss": train_loss / (len(train_dataloader) * batch_size), "val_loss": val_loss / (len(val_dataloader) * batch_size)})
+
+    print(f"Epoch: {epoch} train_loss: {train_loss / (len(train_dataloader)*batch_size)}")
+    print(f"Epoch: {epoch} val_loss: {val_loss / (len(val_dataloader)*batch_size)}")
+
+    if (val_loss / (len(val_dataloader) * batch_size)) < best_validation_loss:
+        best_validation_loss = val_loss
+        best_model = model
+
+# Save the trained model with date and time in the path
+current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+model_path = f"saved_models/{os.path.basename(__file__)}_{current_time}_{wandb.run.name}"
+
+torch.save(best_model, model_path)
+print("Best Model Saved")
+print("Finished Training")
+wandb.finish()
+
+# create
