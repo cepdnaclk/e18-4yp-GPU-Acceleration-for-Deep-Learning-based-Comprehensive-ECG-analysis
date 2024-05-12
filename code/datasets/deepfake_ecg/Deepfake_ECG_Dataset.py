f302a3703c3716d3d7fb5c3ac9e79187357cab75
ridmajayasundara <e18155@eng.pdn.ac.lk>
2024-05-12 21:14:16 +0530

    comment out normalization for convert_to_CH_8_2D_MATRIX_OUTPUT_TYPE

--- code/datasets/deepfake_ecg/Deepfake_ECG_Dataset.py
+++ code/datasets/deepfake_ecg/Deepfake_ECG_Dataset.py
@@ -361,7 +361,7 @@ class Deepfake_ECG_Dataset(torch.utils.data.Dataset):
             ecg_signals = self.convert_to_DEFAULT_OUTPUT_TYPE(ecg_signals)
         elif self.output_type == CH_8_2D_MATRIX_OUTPUT_TYPE:
             ecg_signals = self.convert_to_CH_8_2D_MATRIX_OUTPUT_TYPE(ecg_signals)
-            ecg_signals = self.normaize_each_lead(ecg_signals) # IF NORMALIZATION FOR EACH LEAD IS NOT REQUIRED : COMMENT THIS OUT
+            # ecg_signals = self.normaize_each_lead(ecg_signals) # IF NORMALIZATION FOR EACH LEAD IS NOT REQUIRED : COMMENT THIS OUT
         elif self.output_type == DEFAULT_SPECTROGRAM_OUTPUT_TYPE:
             ecg_signals = self.convert_to_DEFAULT_SPECTROGRAM_OUTPUT_TYPE(ecg_signals)
         elif self.output_type == VISION_TRANSFORMER_IMAGE_OUTPUT_TYPE:
