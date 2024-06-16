f8741733c97b3e28e144dbf42d47f4785a56ddb1
ridmajayasundara <e18155@eng.pdn.ac.lk>
2024-06-16 12:00:26 +0530

    Inception Classification running script updated

--- code/22_Inception1D_classification.py
+++ code/22_Inception1D_classification.py
@@ -1,3 +1,4 @@
+import torch.utils
 import utils.others as others
 
 print(f"Last updated by: ", others.get_latest_update_by())
@@ -15,25 +16,26 @@ import numpy as np
 import random
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
-
-from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.optim.lr_scheduler import ExponentialLR
 
 # Record the start time
 start_time = time.time()
 
 from models.Inception1D import Inception1d
-from datasets.PTB_XL.PTB_XL_ECG_Dataset import ECGDataset, SHAPE_2D
-from datasets.PTB_XL_Plus.PTB_XL_PLUS_ECG_Dataset import PTB_XL_PLUS_ECGDataset, SUB_DATASET_A, SUB_DATASET_B
 
+from datasets.PTB_XL.PTB_XL_ECG_Dataset import ECGDataset, SHAPE_2D
+
+from datasets.PTB_XL_Plus.PTB_XL_PLUS_ECG_Dataset import PTB_XL_PLUS_ECGDataset, SUB_DATASET_A, SUB_DATASET_B
 
 # Hyperparameters
 batch_size = 31
 learning_rate = 0.01
-num_epochs = 5
-train_fraction = 0.8
-select_subdataset = SUB_DATASET_A
+num_epochs = 1000
+train_fraction = 0.8       # so test fraction is 0.2
+val_fraction = 0.1         # val fraction is 0.1 out of the total dataset | 0.125 out of train fraction
+select_sub_dataset = SUB_DATASET_A
 
-patience = 30
+patience = 50
 early_stopping_counter = 0
 best_val_auc_roc = 0
 
@@ -53,24 +55,32 @@ if torch.cuda.is_available():
 # start a new wandb run to track this script
 wandb.init(
     # set the wandb project where this run will be logged
-    project="version3_classification",  #    project="final_runs_by_ridma",
+    project="subset_classification",
     # track hyperparameters and run metadata
-    config={"learning_rate": learning_rate, "architecture": os.path.basename(__file__), "dataset": "PTB-XL", "epochs": num_epochs, "parameter": "classification", "sub_dataset": select_subdataset},
-    notes="classification no transfer learning 1 inception alone",
+    config={
+        "learning_rate": learning_rate, 
+        "architecture": os.path.basename(__file__), 
+        "dataset": "PTB-XL-PLUS", 
+        "epochs": num_epochs, 
+        "parameter": "classification", 
+        "sub_dataset": select_sub_dataset},
+    notes="",
 )
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Create the model
-model = Inception1d(num_classes=5, input_channels=12, use_residual=True, ps_head=0.5, lin_ftrs_head=[128], kernel_size=40).to(device)
+model = Inception1d(num_classes=5, input_channels=8, use_residual=True, ps_head=0.5, lin_ftrs_head=[128], kernel_size=40).to(device)
 
 # Create the dataset class
-dataset = PTB_XL_PLUS_ECGDataset(num_of_leads=12, sub_dataset=select_subdataset, is_classification=True)
+dataset = PTB_XL_PLUS_ECGDataset(num_of_leads=8, sub_dataset=select_sub_dataset, is_classification=True)
 
 
 # Split the dataset into training and validation sets
-train_indices, val_indices = train_test_split(range(len(dataset)), test_size=1 - train_fraction, random_state=42, shuffle=True)
+train_indices, test_indices = train_test_split(range(len(dataset)), test_size=1 - train_fraction, random_state=42, shuffle=True)
+train_indices, val_indices = train_test_split(train_indices, test_size=(val_fraction/train_fraction), random_state=42, shuffle=True)
 
 train_dataset = torch.utils.data.Subset(dataset, train_indices)
 val_dataset = torch.utils.data.Subset(dataset, val_indices)
+test_dataset = torch.utils.data.Subset(dataset, test_indices)
 
 # set num_workers
 if current_server.is_running_in_server():
@@ -83,12 +93,12 @@ else:
 # Create data loaders for training and validation
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
 # Optimizer and loss function
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 criterion = nn.CrossEntropyLoss()  # Use CrossEntropyLoss for multi-class classification
-scheduler = CosineAnnealingLR(optimizer, T_max=20, eta_min=1e-5)
-
+scheduler = ExponentialLR(optimizer, gamma=0.99)  # Set the gamma value for exponential decay
 
 for epoch in range(num_epochs):
     model.train()
@@ -202,8 +212,55 @@ for epoch in range(num_epochs):
 
     # Check if early stopping criteria is met
     if early_stopping_counter >= patience:
-        print(f"Early stopping at epoch {epoch}")
+        print(f"********Early stopping at epoch {epoch}********")
         break
+
+#end of trainging Start of Testing
+print("Using best model for Testing...")
+best_model.eval()
+total_correct = 0
+total_samples = 0
+all_outputs = []
+all_labels = []
+
+with torch.no_grad():
+    for i, data in tqdm(
+        enumerate(test_dataloader, 0),
+        total=len(test_dataloader),
+        desc=f"Testing",
+    ):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        outputs = model(inputs)
+
+        # Calculate accuracy
+        predicted = torch.argmax(outputs, 1)
+        labels_max = torch.argmax(labels, 1)
+        total_correct += (predicted == labels_max).sum().item()
+        total_samples += labels.size(0)
+
+        # Store outputs and labels for AUC-ROC calculation
+        all_outputs.extend(outputs.detach().cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+
+    # Compute accuracy
+    test_accuracy = total_correct / total_samples
+
+    # Compute AUC-ROC
+    all_outputs = np.concatenate(all_outputs, axis=0)
+    all_labels = np.concatenate(all_labels, axis=0)
+    test_auc_roc = roc_auc_score(all_labels, all_outputs)
+
+    # Log metrics
+    print(f"test_accuracy: {test_accuracy}, test_auc_roc: {test_auc_roc}, total_correct: {total_correct}, total_samples: {total_samples}")
+#  Log metrics
+wandb.log(
+    {
+        "test_accuracy": test_accuracy,
+        "test_AUC": test_auc_roc,
+    }
+)
 
 
 # Save the trained model with date and time in the path
